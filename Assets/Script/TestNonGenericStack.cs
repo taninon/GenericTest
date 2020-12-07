a97a91cb10aea9f46fd430a9c4471c20a9e903f1
noriaki_tanimichi <tanimichi@morality-line.com>
2020-12-07 19:15:23 +0900

    ジェネリックじゃない場合テスト

---
+++ Assets/Script/TestNonGenericStack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestNonGenericStack
+{
+	private int stackIndex = 0;
+	private int[] stackArray;
+	private readonly int maxStack = 10;
+
+	public TestNonGenericStack()
+	{
+		stackArray = new int[maxStack];
+	}
+
+	public void Push(int value)
+	{
+		if (stackIndex <= maxStack)
+		{
+			stackArray[stackIndex] = value;
+			stackIndex++;
+			return;
+		}
+
+		Debug.Log("StackMax");
+	}
+
+	public int Pop()
+	{
+		if (stackIndex == 0)
+		{
+			return stackArray[0];
+		}
+
+		stackIndex--;
+		return stackArray[stackIndex];
+	}
+}
+
