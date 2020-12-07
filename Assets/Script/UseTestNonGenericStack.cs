c4846184df93c0125f42ea16244a76968859305a
noriaki_tanimichi <tanimichi@morality-line.com>
2020-12-07 21:17:43 +0900

    Stackを使うテスト

---
+++ Assets/Script/UseTestNonGenericStack.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UseTestNonGenericStack : MonoBehaviour
+{
+	private void Start()
+	{
+		var stack = new TestNonGenericStack();
+		stack.Push(5);
+		stack.Push(2);
+		stack.Push(3);
+
+		Debug.Log(stack.Pop());
+		Debug.Log(stack.Pop());
+		Debug.Log(stack.Pop());
+	}
+
+}
